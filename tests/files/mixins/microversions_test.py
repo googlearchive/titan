6acb85d9d58b8e302f672350e26aeda9e22741b8
Mike Fotinakis <fotinakis@google.com>
2013-01-08 12:42:03 -0800

    Added missing microversions files.

---
+++ tests/files/mixins/microversions_test.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# Copyright 2012 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for microversions.py."""
+
+from tests.common import testing
+
+from google.appengine.api import files as blobstore_files
+from google.appengine.datastore import datastore_stub_util
+
+from titan.common.lib.google.apputils import basetest
+from titan.files import files
+from titan.files.mixins import microversions
+from titan.files.mixins import versions
+from titan.users import users
+
+# Content larger than the task invocation RPC size limit.
+LARGE_FILE_CONTENT = 'a' * (1 << 21)  # 2 MiB
+
+# The class used during the initial action at the root path.
+class MicroversioningMixin(microversions.MicroversioningMixin, files.File):
+
+  @classmethod
+  def ShouldApplyMixin(cls, **kwargs):
+    if kwargs.get('_no_mixins'):
+      return False
+    return microversions.MicroversioningMixin.ShouldApplyMixin(**kwargs)
+
+# The class used during the deferred task action.
+class FileVersioningMixin(versions.FileVersioningMixin, files.File):
+
+  @classmethod
+  def ShouldApplyMixin(cls, **kwargs):
+    if kwargs.get('_no_mixins'):
+      return False
+    return versions.FileVersioningMixin.ShouldApplyMixin(**kwargs)
+
+class MicroversionsTest(testing.BaseTestCase):
+
+  def setUp(self):
+    super(MicroversionsTest, self).setUp()
+    self.vcs = versions.VersionControlService()
+    files.RegisterFileMixins([MicroversioningMixin, FileVersioningMixin])
+
+  def testRootTreeHandling(self):
+    # Actions should check root tree files, not versioning _FilePointers.
+    self.assertFalse(files.File('/foo').exists)
+    files.File('/foo', _no_mixins=True).Write('')
+    self.assertTrue(files.File('/foo').exists)
+    files.File('/foo', _no_mixins=True).Delete()
+    self.assertFalse(files.File('/foo').exists)
+
+    # Write(), and Delete() should all modify root tree files
+    # and defer a versioning task which commits a single-file changeset.
+    files.File('/foo').Write('foo')
+    self.assertEqual(1, len(self.taskqueue_stub.get_filtered_tasks()))
+    self.assertEqual('foo', files.File('/foo', _no_mixins=True).content)
+    files.File('/foo').Delete()
+    self.assertEqual(2, len(self.taskqueue_stub.get_filtered_tasks()))
+    self.assertFalse(files.File('/foo', _no_mixins=True).exists)
+
+    # Verify large RPC deferred task handling.
+    files.File('/foo').Write(LARGE_FILE_CONTENT)
+    self.assertEqual(3, len(self.taskqueue_stub.get_filtered_tasks()))
+    self.assertEqual(
+        LARGE_FILE_CONTENT, files.File('/foo', _no_mixins=True).content)
+
+  def testContentAndBlobsHandling(self):
+    files.File('/foo').Write('foo')
+    files.File('/foo').Delete()
+    # This will immediately go to blobstore, then the deferred task will
+    # have a "blob" argument:
+    files.File('/foo').Write(LARGE_FILE_CONTENT)
+
+    self.LoginTaskAdminUser()
+    self._RunDeferredTasks(microversions.TASKQUEUE_NAME)
+    self.LoginNormalUser()
+
+    # After tasks actually run, verify correct content was saved over time
+    # to the versioned paths:
+    file_versions = self.vcs.GetFileVersions('/foo')
+
+    # In reverse-chronological order:
+    titan_file = files.File(
+        '/foo', changeset=file_versions[0].content_changeset)
+    self.assertEqual(LARGE_FILE_CONTENT, titan_file.content)
+    self.assertEqual('titanuser@example.com', str(titan_file.created_by))
+    self.assertEqual('titanuser@example.com', str(titan_file.modified_by))
+    # Blackbox test: created_by and modified_by might be coming from the
+    # backwards-compatibility code in versions. Verify they are actually
+    # stored correctly.
+    self.assertEqual('titanuser@example.com', str(titan_file._file.created_by))
+    self.assertEqual('titanuser@example.com', str(titan_file._file.modified_by))
+
+    titan_file = files.File(
+        '/foo', changeset=file_versions[1].content_changeset)
+    self.assertEqual('', titan_file.content)
+
+    titan_file = files.File(
+        '/foo', changeset=file_versions[2].content_changeset)
+    self.assertEqual('foo', titan_file.content)
+
+    self.assertEqual(versions.FILE_CREATED, file_versions[0].status)
+    self.assertEqual(versions.FILE_DELETED, file_versions[1].status)
+    self.assertEqual(versions.FILE_CREATED, file_versions[2].status)
+
+  def testCommitMicroversion(self):
+    user = users.TitanUser('test@example.com')
+
+    # Write.
+    final_changeset = microversions._CommitMicroversion(
+        file_kwargs={'path': '/foo'}, method_kwargs={'content': 'foo'},
+        user=user, action=microversions._Actions.WRITE)
+    self.assertEqual(2, final_changeset.num)
+    titan_file = files.File('/foo', changeset=final_changeset.linked_changeset)
+    self.assertEqual('foo', titan_file.content)
+
+    # Verify the final changeset's created_by.
+    self.assertEqual('test@example.com', str(final_changeset.created_by))
+
+    # Write with an existing root file (which should be copied to the version).
+    files.File('/foo', _no_mixins=True).Write('new foo')
+    final_changeset = microversions._CommitMicroversion(
+        file_kwargs={'path': '/foo'}, method_kwargs={'meta': {'color': 'blue'}},
+        user=user, action=microversions._Actions.WRITE)
+    self.assertEqual(4, final_changeset.num)
+    titan_file = files.File('/foo', changeset=final_changeset.linked_changeset)
+    self.assertEqual('new foo', titan_file.content)
+    self.assertEqual('blue', titan_file.meta.color)
+
+    # Delete. In the real code path, the delete of the root file will often
+    # complete before the task is started, so we delete /foo to verify that
+    # deletes don't rely on presence of the root file.
+    files.File('/foo', _no_mixins=True).Delete()
+    final_changeset = microversions._CommitMicroversion(
+        file_kwargs={'path': '/foo'}, method_kwargs={},
+        user=user, action=microversions._Actions.DELETE)
+    self.assertEqual(6, final_changeset.num)
+    titan_file = files.File('/foo', changeset=final_changeset.linked_changeset)
+    self.assertEqual('', titan_file.content)
+
+    # Check file versions.
+    file_versions = self.vcs.GetFileVersions('/foo')
+    self.assertEqual(6, file_versions[0].changeset.num)
+    self.assertEqual(4, file_versions[1].changeset.num)
+    self.assertEqual(2, file_versions[2].changeset.num)
+    self.assertEqual(versions.FILE_DELETED, file_versions[0].status)
+    self.assertEqual(versions.FILE_EDITED, file_versions[1].status)
+    self.assertEqual(versions.FILE_CREATED, file_versions[2].status)
+
+  def testStronglyConsistentCommits(self):
+    user = users.TitanUser('test@example.com')
+
+    # Microversions uses FinalizeAssociatedPaths so the Commit() path should use
+    # the always strongly-consistent GetFiles(), rather than a query. Verify
+    # this behavior by simulating a never-consistent HR datastore.
+    policy = datastore_stub_util.PseudoRandomHRConsistencyPolicy(probability=0)
+    self.testbed.init_datastore_v3_stub(consistency_policy=policy)
+
+    final_changeset = microversions._CommitMicroversion(
+        file_kwargs={'path': '/foo'}, method_kwargs={'content': 'foo'},
+        user=user, action=microversions._Actions.WRITE)
+    self.assertEqual(2, final_changeset.num)
+    titan_file = files.File('/foo', changeset=final_changeset.linked_changeset)
+    self.assertEqual('foo', titan_file.content)
+
+  def testKeepOldBlobs(self):
+    # Create a blob and blob_reader for testing.
+    filename = blobstore_files.blobstore.create(
+        mime_type='application/octet-stream')
+    with blobstore_files.open(filename, 'a') as fp:
+      fp.write('Blobstore!')
+    blobstore_files.finalize(filename)
+    blob_key = blobstore_files.blobstore.get_blob_key(filename)
+
+    # Verify that the blob is not deleted when microversioned content resizes.
+    files.File('/foo').Write(blob=blob_key)
+    self._RunDeferredTasks(microversions.TASKQUEUE_NAME)
+    titan_file = files.File('/foo')
+    self.assertTrue(titan_file.blob)
+    self.assertEqual('Blobstore!', titan_file.content)
+    self._RunDeferredTasks(microversions.TASKQUEUE_NAME)
+    # Resize as smaller (shouldn't delete the old blob).
+    files.File('/foo').Write('foo')
+    files.File('/foo').Write(blob=blob_key)  # Resize back to large size.
+    # Delete file (shouldn't delete the old blob).
+    files.File('/foo').Delete()
+    self._RunDeferredTasks(microversions.TASKQUEUE_NAME)
+
+    file_versions = self.vcs.GetFileVersions('/foo')
+
+    # Deleted file (blob should be None).
+    changeset = file_versions[0].changeset.linked_changeset
+    titan_file = files.File('/foo', changeset=changeset)
+    self.assertIsNone(titan_file.blob)
+
+    # Created file (blob key and blob content should still exist).
+    changeset = file_versions[-1].changeset.linked_changeset
+    titan_file = files.File('/foo', changeset=changeset)
+    self.assertTrue(titan_file.blob)
+    self.assertEqual('Blobstore!', titan_file.content)
+
+if __name__ == '__main__':
+  basetest.main()
---
+++ titan/files/mixins/microversions.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# Copyright 2012 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Wrapper around versions which auto-commits all file changes.
+
+http://code.google.com/p/titan-files/wiki/Microversions
+"""
+
+from google.appengine.ext import deferred
+
+from titan.common import utils
+from titan.files import files
+from titan.files.mixins import versions
+from titan.users import users
+
+TASKQUEUE_NAME = 'microversions'
+
+class _Actions(object):
+  WRITE = 'write'
+  DELETE = 'delete'
+
+class MicroversioningMixin(files.File):
+  """Mixin to provide microversioning of all file actions."""
+
+  @classmethod
+  def ShouldApplyMixin(cls, **kwargs):
+    # Disable if the versions mixin will be enabled, otherwise enable.
+    # Microversioning can be limited to certain paths by making and
+    # registering a subclass of this mixin and overriding this method.
+    mixin_state = kwargs.get('_mixin_state')
+    if ('changeset' in kwargs
+        or mixin_state and mixin_state.get('is_versions_enabled')):
+      return False
+    if mixin_state is not None:
+      mixin_state['is_microversions_enabled'] = True
+    return True
+
+  @utils.ComposeMethodKwargs
+  def Write(self, **kwargs):
+    """Write method. See superclass docstring."""
+    # If content is big enough, write the content to blobstore earlier and pass
+    # the blob to both the superclass and to the deferred task. This allows
+    # large files to be microversioned AND to share the same exact blob between
+    # the root file and the microversioned file.
+    #
+    # Duplicate some method calls from files.File.Write:
+    # If given unicode, encode it as UTF-8 and flag it for future decoding.
+    kwargs['content'], kwargs['encoding'] = self._MaybeEncodeContent(
+        kwargs['content'], kwargs['encoding'])
+    # If big enough, store content in blobstore. Must come after encoding.
+    kwargs['content'], kwargs['blob'] = self._MaybeWriteToBlobstore(
+        kwargs['content'], kwargs['blob'])
+
+    # Writes should go to the root tree, and versioning is deferred.
+    kwargs['_delete_old_blob'] = False
+    file_kwargs = self._original_kwargs.copy()
+    file_kwargs.update({'path': self.path})
+    deferred.defer(
+        _CommitMicroversion,
+        file_kwargs=file_kwargs,
+        method_kwargs=kwargs.copy(),
+        user=users.GetCurrentUser(),
+        action=_Actions.WRITE,
+        # The queue in which this task should run.
+        _queue=TASKQUEUE_NAME)
+    return super(MicroversioningMixin, self).Write(**kwargs)
+
+  @utils.ComposeMethodKwargs
+  def Delete(self, **kwargs):
+    """Delete method. See superclass docstring."""
+    kwargs['_delete_old_blob'] = False
+    file_kwargs = self._original_kwargs.copy()
+    file_kwargs.update({'path': self.path})
+    deferred.defer(
+        _CommitMicroversion,
+        file_kwargs=file_kwargs,
+        method_kwargs=kwargs.copy(),
+        user=users.GetCurrentUser(),
+        action=_Actions.DELETE,
+        # The queue in which this task should run.
+        _queue=TASKQUEUE_NAME)
+    return super(MicroversioningMixin, self).Delete(**kwargs)
+
+def _CommitMicroversion(file_kwargs, method_kwargs, user, action,
+                        created_by=None):
+  """Task to enqueue for microversioning a file action.
+
+  Args:
+    file_kwargs: A dictionary of file keyword args, including 'path'.
+    method_kwargs: A dictionary (or None) of method keyword args.
+    user: A users.TitanUser object.
+    action: The action to perform from the _Actions enum.
+    created_by: Legacy arg; see TODO below.
+  Returns:
+    The final changeset.
+  """
+  # TODO(user): This is backwards-compatibility for in-flight tasks after
+  # the user arg change is first deployed. Feel free to remove this and the
+  # created_by arg after old tasks have processed.
+  if created_by:
+    user = created_by
+
+  vcs = versions.VersionControlService()
+  changeset = vcs.NewStagingChangeset(created_by=user)
+
+  # Setting the 'changeset' argument here is noticed by the factory and so
+  # microversioning will not be mixed in, but versioning will.
+  file_kwargs['changeset'] = changeset
+  file_kwargs['_created_by_override'] = user
+  file_kwargs['_modified_by_override'] = user
+
+  if action == _Actions.WRITE:
+    method_kwargs['_delete_old_blob'] = False
+    files.File(**file_kwargs).Write(**method_kwargs)
+  elif action == _Actions.DELETE:
+    # Mark this file as deleted in the version control system.
+    method_kwargs['delete'] = True
+    method_kwargs['_delete_old_blob'] = False
+    files.File(**file_kwargs).Write(**method_kwargs)
+
+  # Indicate that this specific changeset object has been used for all file
+  # operations and can be trusted for strong consistency guarantees.
+  changeset.FinalizeAssociatedFiles()
+
+  return vcs.Commit(changeset)
